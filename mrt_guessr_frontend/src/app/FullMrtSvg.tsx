91321d4a719c02be7d175f687270145c22f71185
Darren Sim <darrensim999@gmail.com>
2024-05-20 01:32:07 +0800

    Partition mrt, lrt and background

---
+++ mrt_guessr_frontend/src/app/FullMrtSvg.tsx
@@ -0,0 +1,50 @@
+import styles from "./MrtMap.css";
+import LrtSvg from "./LrtSvg";
+import MrtSvg from "./MrtSvg";
+import MrtBackgroundSvg from "./MrtBackgroundSvg";
+
+const {
+  e,
+  f,
+  g,
+  h,
+  i,
+  j,
+  k,
+  l,
+  m,
+  n,
+  o,
+  p,
+  q,
+  r,
+  s,
+  t,
+  u,
+  v,
+  w,
+  x,
+  y,
+  aa,
+  ab,
+  ac,
+  ad,
+  ae,
+  af,
+  ag,
+  ah,
+  ai,
+  aj,
+} = styles;
+
+const FullMrtSvg = () => {
+  return (
+    <div>
+      {/* <MrtBackgroundSvg /> */}
+      <MrtSvg />
+      {/* <LrtSvg /> */}
+    </div>
+  );
+};
+
+export default FullMrtSvg;
--- mrt_guessr_frontend/src/app/page.tsx
+++ mrt_guessr_frontend/src/app/page.tsx
@@ -1,11 +1,10 @@
 import Image from "next/image";
-import MrtSvg from "./MrtSvg";
+import FullMrtSvg from "./FullMrtSvg";
 
 export default function Home() {
   return (
     <div>
-      <h1>Guessr</h1>
-      <MrtSvg />
+      <FullMrtSvg />
     </div>
   );
 }
