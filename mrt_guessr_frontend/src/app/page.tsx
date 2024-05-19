909fbe2ef4f2897496342dfe5176f727048c573a
Darren Sim <e0725166@u.nus.edu>
2024-05-19 22:01:02 +0800

    Add labelled mrt svg

--- mrt_guessr_frontend/src/app/page.tsx
+++ mrt_guessr_frontend/src/app/page.tsx
@@ -5,13 +5,6 @@ export default function Home() {
   return (
     <div>
       <h1>Guessr</h1>
-      {/* <Image
-        src="/mrt_map.svg"
-        alt="Mrt Map"
-        width={1000}
-        height={1000}
-        priority
-      /> */}
       <MrtSvg />
     </div>
   );
